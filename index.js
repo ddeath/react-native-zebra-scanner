147ab4f8b8e6fc37391e0fbe926a44e3921ff57e
Dušan Plavák <dusan.plavak@gmail.com>
2017-10-03 11:37:18 +0200

    Added modifications

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { NativeModules, View, Text, requireNativeComponent } from 'react-native'
 
 const { ZebraScanner } = NativeModules;
 
-const Scanner = requireNativeComponent('ZebraScanner', {});
+const Scanner = requireNativeComponent('ZebraScanner', Zebra, {});
 
 export default class Zebra extends Component {
   render() {
